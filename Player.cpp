c2b69e401b9b5d0489ec6c5e45cde05bb6cba61d
Joseph Yang <joeyang095@gmail.com>
2018-01-12 03:04:03 -0500

    User input registers on entire board

--- Player.cpp
+++ Player.cpp
@@ -10,23 +10,6 @@ char Player::get_sign() {
 	return sign;
 }
 
-/*
-Draws Sprite
-*/
-
-/*
-void Player::draw(int x, int y) { //parameters int x, int y
-	if(sign == 'x') {
-		amount++;
-		dbSprite(amount,x,y,12);
-	}
-	if(sign == 'o') {
-		amount+=2;
-		dbSprite(amount,x,y,11);
-	}
-}
-*/
-
 int Player::get_amount() {
 	return amount;
 }
